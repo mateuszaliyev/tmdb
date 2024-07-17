a8ad82e0455ae758e9f469aedb91611c88bb2c1a
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2024-07-17 19:28:41 +0200

    chore: configure prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,44 @@
+import packageJson from "./package.json" assert { type: "json" };
+
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^react",
+    "^next",
+    "<THIRD_PARTY_MODULES>",
+    "^(@/app)(.*|$)",
+    "^(@/assets)(.*|$)",
+    "^(@/components)(.*|$)",
+    "^(@/constants)(.*|$)",
+    "^(@/environment)(.*|$)",
+    "^(@/hooks)(.*|$)",
+    "^(@/types)(.*|$)",
+    "^(@/utilities)(.*|$)",
+    "^[.]",
+  ].reduce((result, element, index) => {
+    if (index) result.push("", element);
+    else result.push(element);
+    return result;
+  }, /** @type {string[]} */ ([])),
+  importOrderTypeScriptVersion: packageJson.devDependencies.typescript,
+  jsxSingleQuote: false,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  tailwindFunctions: ["cva", "cx"],
+  trailingComma: "all",
+  useTabs: false,
+};
+
+export default prettierConfig;
